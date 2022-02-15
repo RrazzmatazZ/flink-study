377e84277c952b6934a8e63fdc93abd4082f9984
RrazzmatazZ <32166435+RrazzmatazZ@users.noreply.github.com>
2022-02-15 17:59:26 +0800

    flink window function

---
+++ src/main/java/com/example/flink/core/window/function/MyAllowLatenessFunction.java
@@ -0,0 +1,14 @@
+package com.example.flink.core.window.function;
+
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+/**
+ * TODO 待水位线梳理完补充
+ */
+public class MyAllowLatenessFunction {
+
+    public static void main(String[] args) {
+
+    }
+}
--- src/main/java/com/example/flink/core/window/function/MyTriggerFunction.java
+++ src/main/java/com/example/flink/core/window/function/MyTriggerFunction.java
@@ -2,6 +2,8 @@ package com.example.flink.core.window.function;
 
 import com.example.flink.core.common.beans.Person;
 import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.java.tuple.Tuple;
+import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.WindowedStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
@@ -12,7 +14,7 @@ import org.apache.flink.streaming.api.windowing.triggers.TriggerResult;
 import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
 
 /**
- *
+ * 自定义trigger
  */
 public class MyTriggerFunction {
 
@@ -20,40 +22,45 @@ public class MyTriggerFunction {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         DataStreamSource<String> socket = env.socketTextStream("dev03", 4444);
 
-        WindowedStream<Person, String, TimeWindow> trigger = socket.map(new MapFunction<String, Person>() {
+        WindowedStream<Tuple2<Integer, Integer>, Tuple, TimeWindow> trigger = socket
+                .map(new MapFunction<String, Tuple2<Integer, Integer>>() {
                     @Override
-                    public Person map(String value) throws Exception {
-                        return null;
+                    public Tuple2<Integer, Integer> map(String value) throws Exception {
+                        return new Tuple2<>(Integer.parseInt(value), 1);
                     }
                 })
-                .keyBy(Person::getSex)
-                .window(TumblingProcessingTimeWindows.of(Time.seconds(60)))
+                .keyBy(0)
+                .window(TumblingProcessingTimeWindows.of(Time.seconds(20)))
                 .trigger(new MyTrigger());
 
+        trigger.sum(1).print();
 
         env.execute();
     }
 
-    public static class MyTrigger extends Trigger<Person, TimeWindow> {
+    public static class MyTrigger extends Trigger<Tuple2<Integer, Integer>, TimeWindow> {
 
+        //在有元素进入的时候进行的操作
         @Override
-        public TriggerResult onElement(Person element, long timestamp, TimeWindow window, TriggerContext ctx) throws Exception {
-            return null;
+        public TriggerResult onElement(Tuple2<Integer, Integer> element, long timestamp, TimeWindow window, TriggerContext ctx) throws Exception {
+            return TriggerResult.CONTINUE;
         }
 
+        //在ProcessTime执行
         @Override
         public TriggerResult onProcessingTime(long time, TimeWindow window, TriggerContext ctx) throws Exception {
-            return null;
+            return TriggerResult.FIRE;
         }
 
+        //在EventTime执行
         @Override
         public TriggerResult onEventTime(long time, TimeWindow window, TriggerContext ctx) throws Exception {
-            return null;
+            return TriggerResult.CONTINUE;
         }
 
         @Override
         public void clear(TimeWindow window, TriggerContext ctx) throws Exception {
-
+            ctx.deleteProcessingTimeTimer(window.maxTimestamp());
         }
     }
 }
